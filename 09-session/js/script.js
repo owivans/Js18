2555966fb6bdaada9e32be89b217cbcec7a2eb47
owivans <ivan.diaz@nowports.com>
2022-04-20 19:50:37 -0500

    add class

---
+++ 09-session/js/script.js
@@ -0,0 +1,95 @@
+// -obtener el score promeddio de cada materia (HTML, CSS, JS, ReactJS)
+
+// -obtener el promedio individual de cada mentor
+// -crear un array de strings con la siguiente forma:
+
+//     "Mi nombre es {nombre} y mi promedio es de {promedio}"
+// -obtener la lista de mentores cuyo promedio sea mayor a 9.5 
+
+
+const mentorsArray = [
+  {
+      name:"Ivan",
+      scores:[
+          {
+              signature:"HTML",
+              score:8
+          },
+          {
+              signature:"CSS",
+              score:10
+          },
+          {
+              signature:"JS",
+              score:8
+          },
+          {
+              signature:"ReactJS",
+              score:8
+          }
+      ]
+  },
+  {
+      name:"Alejandra",
+      scores:[
+          {
+              signature:"HTML",
+              score:8
+          },
+          {
+              signature:"CSS",
+              score:7
+          },
+          {
+              signature:"JS",
+              score:10
+          },
+          {
+              signature:"ReactJS",
+              score:10
+          }
+      ]
+  },
+  {
+      name:"Alan",
+      scores:[
+          {
+              signature:"HTML",
+              score:9
+          },
+          {
+              signature:"CSS",
+              score:9
+          },
+          {
+              signature:"JS",
+              score:10
+          },
+          {
+              signature:"ReactJS",
+              score:9
+          }
+      ]
+  },
+  {
+      name:"Oscar",
+      scores:[
+          {
+              signature:"HTML",
+              score:8
+          },
+          {
+              signature:"CSS",
+              score:10
+          },
+          {
+              signature:"JS",
+              score:9
+          },
+          {
+              signature:"ReactJS",
+              score:10
+          }
+      ]
+  }
+]
