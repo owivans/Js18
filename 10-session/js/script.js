c22f0f0224e82ff8280edc7f4c1f0b73e0adfed6
owivans <ivan.diaz@nowports.com>
2022-04-27 00:20:31 -0500

    was added new class

--- 10-session/js/script.js
+++ 10-session/js/script.js
@@ -66,7 +66,7 @@ const nombres = personas.map((item) => {
 
 const mentorsArray = [
   {
-      name:"Ivan",
+      name:"Ivan Diaz Alarcon",
       scores:[
           {
               signature:"HTML",
@@ -87,7 +87,7 @@ const mentorsArray = [
       ]
   },
   {
-      name:"Alejandra",
+      name:"Alejandra Saenz Gonzalez",
       scores:[
           {
               signature:"HTML",
@@ -108,7 +108,7 @@ const mentorsArray = [
       ]
   },
   {
-      name:"Alan",
+      name:"Alan Hernandex Hernandex",
       scores:[
           {
               signature:"HTML",
@@ -129,7 +129,7 @@ const mentorsArray = [
       ]
   },
   {
-      name:"Oscar",
+      name:"Raul Aguirre Salazar",
       scores:[
           {
               signature:"HTML",
@@ -150,7 +150,7 @@ const mentorsArray = [
       ]
   },
   {
-    name:"Pepito",
+    name:"Pepito Potter Ron",
     scores:[
         {
             signature:"HTML",
@@ -172,30 +172,36 @@ const mentorsArray = [
 }
 ]
 
-const resultadoDeMentores = mentorsArray.map(( mentor ) => {
-    let accum = 0;
-    mentor.scores.forEach((calificacion) => {
-        accum += calificacion.score
-    });
-    const promedio = accum / mentor.scores.length;
-
-    const mentorInfo = {
-        nombre: mentor.name,
-        promedio,
-    };
-
-    return mentorInfo;
-});
-
-const aprovados = resultadoDeMentores.filter(( promedio ) => {
-    if(promedio.promedio > 9) {
-        return promedio;
+const personajes = [
+    {
+        name: 'ron whisley'
     }
-});
+]
+
+// const resultadoDeMentores = mentorsArray.map(( mentor ) => {
+//     let accum = 0;
+//     mentor.scores.forEach((calificacion) => {
+//         accum += calificacion.score
+//     });
+//     const promedio = accum / mentor.scores.length;
+
+//     const mentorInfo = {
+//         nombre: mentor.name,
+//         promedio,
+//     };
+
+//     return mentorInfo;
+// });
+
+// const aprovados = resultadoDeMentores.filter(( promedio ) => {
+//     if(promedio.promedio > 9) {
+//         return promedio;
+//     }
+// });
 
 
-console.log(resultadoDeMentores);
-console.log(aprovados);
+// console.log(resultadoDeMentores);
+// console.log(aprovados);
 // [
 //     {
 //         nombre: 'asdas',
@@ -211,6 +217,19 @@ console.log(aprovados);
 // - Crear un nuevo arreglo con los mentores cuyo nombre inicien con una vocal 
 // - Crear un nuevo arreglo con las inciales de cada mentor 
 
+
+
+
+
+  const resultado = mentorsArray.filter(( mentor ) => {
+    if(mentor.name[0].match(/[aeiou]/i)){
+        return mentor;
+    }
+  });
+
+//   console.log(resultado)
+
+
 // input : [
 //     'Ivan Diaz Alarcon'
 // ]
@@ -219,7 +238,177 @@ console.log(aprovados);
 //     I.D.A
 // ] 
 
+const formatearNombre = (mentores) => {
+
+
+    const result = mentores.map((item) => {
+        const arrayName = item.name.split(" ");
+        let accum = '';
+
+        arrayName.forEach((firtsLetter) => {
+            accum += firtsLetter.charAt(0) + '.'
+        });
+
+        return accum;
+    });
+
+    return result;
+};
+
+const formatted = formatearNombre(personajes)
+// console.log(formatted)
+
+const numbers = [1, 5, 9, 6, 2, 4];
+
+
+const resultReduce = numbers.reduce(( accum, item, index, arr ) => {
+    // console.log(accum)
+    return item % 2 === 0 ? accum + item : accum;
+}, 0);
+
+// console.log(resultReduce)
 
 
 
+// const resultadoDeMentores = mentorsArray.map(( mentor ) => {
+//     let accum = 0;
+//     mentor.scores.forEach((calificacion) => {
+//         accum += calificacion.score
+//     });
+//     const promedio = accum / mentor.scores.length;
+
+//     const mentorInfo = {
+//         nombre: mentor.name,
+//         promedio,
+//     };
+
+//     return mentorInfo;
+// });
+
+
+const mentorsList = mentorsArray.map(( item, index) => {
+    const { scores } = item;
+    const resultMentor = scores.reduce(( accum, item, index, arr) => {
+        return accum + item.score
+    }, 0)
+
+    const primedio = resultMentor / item.scores.length;
+
+    return primedio;
+});
+
+// console.log(mentorsList)
+
+
+
+
+const estudiantes = [
+    {
+        nombre: 'jason',
+        edad: 18,
+    },
+    {
+        nombre: 'freddy',
+        edad: 9,
+    },
+    {
+        nombre: 'Penny',
+        edad: 21,
+    }, 
+    {
+        nombre: 'Arnold',
+        edad: 6,
+    }
+];
+
+
+
+// spread operator
+// const mergeArray = [ ...estudiantes, ...estudiantesExternos ];
+
+
+const personaInfo = {
+    name: 'asdas',
+    lastName: 'lastname'
+}
+
+const details = {
+    height: 1.90,
+    age: 20,
+    name: 'ivan',
+    lastName: 'Diaz',
+    direccion:{
+        calle: 'calle cualquera',
+        numero: 123
+    },
+}
+
+// destructuring Objetos
+const name = 'otherName'
+const { direccion,  lastName, age, name: nombre } = details;
+
+// details.name
+// details.age
+// details.lastName
+
+// console.log(direccion, lastName, age, nombre,  'lastName destrucuturado')
+
+const estudiantesExternos = [
+    {
+        nombre: 'Gerald',
+        edad: 6,
+    }, 
+    {
+        nombre: 'Gelga',
+        edad: 6,
+    }
+]
+
+const [ primerEstudiante, segundoEstudiante, tercerEstudiante ] = estudiantesExternos;
+console.log(tercerEstudiante, 'primerEstudiante');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const mergeObjet = {...personaInfo, ...details, test: 'asd', test2: 'asda' }
+// console.log(mergeObjet, 'mergeObjet')
+
+
+
+const estudiantesAdultos = (estudiantesList) => {
+    const result = estudiantesList.reduce((accum, item) => {
+        if(item.edad >= 18) {
+            return [ ...accum, item ]
+        }
+        return accum
+    }, []);
+
+    return result;
+};
+
+const filtro = estudiantesAdultos(estudiantes)
+console.log(filtro)
+
+// [
+//     {
+//         nombre: 'Penny',
+//         edad: 21,
+//     },
+
+// ]
+
+
+'asdasd'.charAt(0)
 
